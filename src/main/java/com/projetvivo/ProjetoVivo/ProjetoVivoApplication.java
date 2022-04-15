11be01beaa27b325653934b05755032ef5da3663
jeffy271189@gmail.com <jeffy271189@gmail.com>
2022-04-15 10:26:02 -0300

    first commit

---
+++ src/main/java/com/projetvivo/ProjetoVivo/ProjetoVivoApplication.java
@@ -0,0 +1,13 @@
+package com.projetvivo.ProjetoVivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoVivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetvivo/ProjetoVivo/ProjetoVivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetvivo.ProjetoVivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoVivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
