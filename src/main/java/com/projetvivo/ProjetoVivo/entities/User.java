a01d9e59d44d4ed5179b5c90755127f3ce62f89c
jeffy271189@gmail.com <jeffy271189@gmail.com>
2022-04-15 11:50:43 -0300

    Signed-off-by: jeffy271189@gmail.com <jeffy271189@gmail.com>

---
+++ src/main/java/com/projetvivo/ProjetoVivo/entities/User.java
@@ -0,0 +1,80 @@
+package com.projetvivo.ProjetoVivo.entities;
+
+public class User {
+	
+	private Long id; 
+	private String nome;
+	private String email; 
+	private String password;
+	private String address;
+	private Long telefone; 
+	
+	
+	public User () {
+	
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public Long getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(Long telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+
+}
